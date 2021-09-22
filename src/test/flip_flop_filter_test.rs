1ca35433919d7c23982fec0ed521ee1f7af2392c
Linas Nikiperavičius <linas@linasdev.com>
2021-09-22 18:40:50 +0300

    Add flip flop filter tests

---
+++ src/test/flip_flop_filter_test.rs
@@ -0,0 +1,36 @@
+use crate::Value;
+use crate::filter::*;
+use crate::state::StateManager;
+
+const VALUE_1: u8 = 0xff;
+
+#[test]
+fn flip_flop_filter_initial_false_test() {
+    let mut state_manager = StateManager::new();
+    let mut filter = FlipFlopFilter::new(false);
+
+    assert_eq!(filter.filter(&Value::None, &mut state_manager), true);
+    assert_eq!(filter.filter(&Value::None, &mut state_manager), false);
+    assert_eq!(filter.filter(&Value::None, &mut state_manager), true);
+    assert_eq!(filter.filter(&Value::None, &mut state_manager), false);
+}
+
+#[test]
+fn flip_flop_filter_initial_true_test() {
+    let mut state_manager = StateManager::new();
+    let mut filter = FlipFlopFilter::new(true);
+
+    assert_eq!(filter.filter(&Value::None, &mut state_manager), false);
+    assert_eq!(filter.filter(&Value::None, &mut state_manager), true);
+    assert_eq!(filter.filter(&Value::None, &mut state_manager), false);
+    assert_eq!(filter.filter(&Value::None, &mut state_manager), true);
+}
+
+#[test]
+#[should_panic(expected = "Wrong value provided for flip flop filter.")]
+fn u16_is_equal_filter_value_has_bad_type_test() {
+    let mut state_manager = StateManager::new();
+    let mut filter = FlipFlopFilter::new(true);
+
+    filter.filter(&Value::U8(VALUE_1), &mut state_manager);
+}
--- src/test/mod.rs
+++ src/test/mod.rs
@@ -1,1 +1,2 @@
 mod u16_filter_test;
+mod flip_flop_filter_test;
