76ed98d7b23adabf7958589135ef349cf4113c0b
Linas Nikiperavičius <linas@linasdev.com>
2021-09-22 20:04:50 +0300

    Add u32 is equal state filter tests

--- src/test/filter_test/state_filter_test/u32_state_filter_test.rs
+++ src/test/filter_test/state_filter_test/u32_state_filter_test.rs
@@ -4,7 +4,50 @@ use crate::filter::state_filter::*;
 use crate::state::StateManager;
 
 const VALUE_1: u32 = 0x0000_0000;
-const VALUE_2: u8 = 0xff;
+const VALUE_2: u32 = 0xabab_abab;
+const VALUE_3: u8 = 0xff;
+
+#[test]
+fn u32_is_equal_state_filter_values_equal_test() {
+    let mut state_manager = StateManager::new();
+    let state_index = state_manager.add_state(Value::U32(VALUE_1));
+
+    let mut filter = U32IsEqualStateFilter::new(state_index, VALUE_1);
+
+    assert_eq!(filter.filter(&Value::None, &mut state_manager), true);
+}
+
+#[test]
+fn u32_is_equal_state_filter_values_not_equal_test() {
+    let mut state_manager = StateManager::new();
+    let state_index = state_manager.add_state(Value::U32(VALUE_1));
+
+    let mut filter = U32IsEqualStateFilter::new(state_index, VALUE_2);
+
+    assert_eq!(filter.filter(&Value::None, &mut state_manager), false);
+}
+
+#[test]
+#[should_panic(expected = "Wrong value provided for u32 is equal state filter.")]
+fn u32_is_equal_state_filter_value_has_bad_type_test() {
+    let mut state_manager = StateManager::new();
+    let state_index = state_manager.add_state(Value::U32(VALUE_1));
+
+    let mut filter = U32IsEqualStateFilter::new(state_index, VALUE_2);
+
+    filter.filter(&Value::U32(VALUE_1), &mut state_manager);
+}
+
+#[test]
+#[should_panic(expected = "Wrong state value provided for u32 is equal state filter.")]
+fn u32_is_equal_state_filter_state_value_has_bad_type_test() {
+    let mut state_manager = StateManager::new();
+    let state_index = state_manager.add_state(Value::U8(VALUE_3));
+
+    let mut filter = U32IsEqualStateFilter::new(state_index, VALUE_2);
+
+    filter.filter(&Value::None, &mut state_manager);
+}
 
 #[test]
 fn u32_increment_state_filter_initial_zero_test() {
@@ -40,7 +83,7 @@ fn u32_increment_state_filter_value_has_bad_type_test() {
 
     let mut filter = U32IncrementStateFilter::new(state_index);
 
-    filter.filter(&Value::U8(VALUE_2), &mut state_manager);
+    filter.filter(&Value::U8(VALUE_3), &mut state_manager);
 }
 
 
@@ -48,7 +91,7 @@ fn u32_increment_state_filter_value_has_bad_type_test() {
 #[should_panic(expected = "Wrong state value provided for u32 increment state filter.")]
 fn u32_increment_state_filter_state_value_has_bad_type_test() {
     let mut state_manager = StateManager::new();
-    let state_index = state_manager.add_state(Value::U8(VALUE_2));
+    let state_index = state_manager.add_state(Value::U8(VALUE_3));
 
     let mut filter = U32IncrementStateFilter::new(state_index);
 
