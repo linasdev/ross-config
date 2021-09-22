35e5cc962c0c66f409ef1e1387c7e8d024da892d
Linas Nikiperavičius <linas@linasdev.com>
2021-09-22 19:52:07 +0300

    Derive Debug and PartialEq

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,12 @@ pub mod state;
 #[cfg(test)]
 mod test;
 
+#[derive(Debug, PartialEq)]
 pub struct DeviceInfo {
     device_address: u16,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Value {
     None,
     U8(u8),
