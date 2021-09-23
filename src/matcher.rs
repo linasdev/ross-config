27532bc0d278f89e3d814d43f0d582ed0ce764ec
Linas Nikiperavičius <linas@linasdev.com>
2021-09-23 18:57:25 +0300

    Make matcher fields public

--- src/matcher.rs
+++ src/matcher.rs
@@ -9,15 +9,11 @@ use crate::filter::Filter;
 use crate::state::StateManager;
 
 pub struct Matcher {
-    extractor: Box<dyn Extractor>,
-    filter: Box<dyn Filter>,
+    pub extractor: Box<dyn Extractor>,
+    pub filter: Box<dyn Filter>,
 }
 
 impl Matcher {
-    pub fn new(extractor: Box<dyn Extractor>, filter: Box<dyn Filter>) -> Self {
-        Self { extractor, filter }
-    }
-
     pub fn do_match(&mut self, packet: &Packet, state_manager: &mut StateManager) -> bool {
         let value = self.extractor.extract(packet);
 
