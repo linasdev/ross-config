f80c5dee048408a2ba42b917c32d8cae259dd15f
Linas Nikiperavičius <linas@linasdev.com>
2021-11-27 16:18:07 +0200

    Rename some modules

--- src/extractor/mod.rs
+++ src/extractor/mod.rs
@@ -6,26 +6,26 @@ use ross_protocol::packet::Packet;
 
 use crate::ExtractorValue;
 
-mod none_extractor;
-pub use none_extractor::*;
+mod none;
+pub use none::*;
 
-mod event_code_extractor;
-pub use event_code_extractor::*;
+mod event_code;
+pub use event_code::*;
 
-mod packet_extractor;
-pub use packet_extractor::*;
+mod packet;
+pub use packet::*;
 
-mod event_producer_address_extractor;
-pub use event_producer_address_extractor::*;
+mod event_producer_address;
+pub use event_producer_address::*;
 
-mod message_code_extractor;
-pub use message_code_extractor::*;
+mod message_code;
+pub use message_code::*;
 
-mod message_value_extractor;
-pub use message_value_extractor::*;
+mod message_value;
+pub use message_value::*;
 
-mod button_index_extractor;
-pub use button_index_extractor::*;
+mod button_index;
+pub use button_index::*;
 
 pub const NONE_EXTRACTOR_CODE: u16 = 0x0000;
 pub const EVENT_CODE_EXTRACTOR_CODE: u16 = 0x0001;
