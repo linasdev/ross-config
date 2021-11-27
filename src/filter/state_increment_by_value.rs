3412149e8c905dd512a2c42bf1ddf0a9ae6528ea
Linas Nikiperavičius <linas@linasdev.com>
2021-11-27 17:19:56 +0200

    Add state increment by value filter

---
+++ src/filter/state_increment_by_value.rs
@@ -0,0 +1,117 @@
+use crate::filter::{Filter, FilterError};
+use crate::state_manager::StateManager;
+use crate::{ExtractorValue, Value};
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct StateIncrementByValueFilter {
+    state_index: u32,
+}
+
+impl StateIncrementByValueFilter {
+    pub fn new(state_index: u32) -> Self {
+        Self {
+            state_index,
+        }
+    }
+}
+
+impl Filter for StateIncrementByValueFilter {
+    fn filter(
+        &mut self,
+        value: &ExtractorValue,
+        state_manager: &mut StateManager,
+    ) -> Result<bool, FilterError> {
+        let increment_value = match *value {
+            ExtractorValue::U8(increment_value) => Value::U8(increment_value),
+            ExtractorValue::U16(increment_value) => Value::U16(increment_value),
+            ExtractorValue::U32(increment_value) => Value::U32(increment_value),
+            _ => return Err(FilterError::WrongValueType),
+        };
+
+        let new_value = match (
+            state_manager.get_value(self.state_index),
+            increment_value,
+        ) {
+            (Some(Value::U8(current_state)), Value::U8(increment_value)) => {
+                Value::U8(current_state.wrapping_add(increment_value))
+            }
+            (Some(Value::U16(current_state)), Value::U16(increment_value)) => {
+                Value::U16(current_state.wrapping_add(increment_value))
+            }
+            (Some(Value::U32(current_state)), Value::U32(increment_value)) => {
+                Value::U32(current_state.wrapping_add(increment_value))
+            }
+            _ => return Err(FilterError::WrongStateType),
+        };
+
+        state_manager.set_value(self.state_index, new_value);
+
+        Ok(true)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn initial_zero_increment_by_five_test() {
+        let mut state_manager = StateManager::new();
+        state_manager.set_value(0, Value::U32(0x0000_0000));
+
+        let mut filter = StateIncrementByValueFilter::new(0);
+
+        assert_eq!(
+            filter.filter(&ExtractorValue::U32(0x0000_0005), &mut state_manager),
+            Ok(true),
+        );
+        assert_eq!(
+            *state_manager.get_value(0).unwrap(),
+            Value::U32(0x0000_0005),
+        );
+    }
+
+    #[test]
+    fn initial_max_increment_by_one_test() {
+        let mut state_manager = StateManager::new();
+        state_manager.set_value(0, Value::U32(0xffff_ffff));
+
+        let mut filter = StateIncrementByValueFilter::new(0);
+
+        assert_eq!(
+            filter.filter(&ExtractorValue::U32(0x0000_0001), &mut state_manager),
+            Ok(true),
+        );
+        assert_eq!(
+            *state_manager.get_value(0).unwrap(),
+            Value::U32(0x0000_0000),
+        );
+    }
+
+    #[test]
+    fn wrong_state_type_test() {
+        let mut state_manager = StateManager::new();
+        state_manager.set_value(0, Value::U8(0x00));
+
+        let mut filter = StateIncrementByValueFilter::new(0);
+
+        assert_eq!(
+            filter.filter(&ExtractorValue::U32(0x0000_0001), &mut state_manager),
+            Err(FilterError::WrongStateType)
+        );
+    }
+
+    #[test]
+    fn wrong_value_type_test() {
+        let mut state_manager = StateManager::new();
+        state_manager.set_value(0, Value::U8(0x00));
+
+        let mut filter = StateIncrementByValueFilter::new(0);
+
+        assert_eq!(
+            filter.filter(&ExtractorValue::None, &mut state_manager),
+            Err(FilterError::WrongValueType)
+        );
+    }
+}
