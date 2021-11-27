74e08e84a8db2ac06fb2b5549198fc89aaa0485f
Linas Nikiperavičius <linas@linasdev.com>
2021-11-27 15:35:23 +0200

    Rethink what filters are gonna be needed

--- src/filter/mod.rs
+++ src/filter/mod.rs
@@ -4,34 +4,38 @@ use downcast_rs::{impl_downcast, Downcast};
 use crate::state::StateManager;
 use crate::ExtractorValue;
 
-pub mod state;
+mod value_equal_to_const;
+pub use value_equal_to_const::*;
 
-mod u16_filter;
-pub use u16_filter::*;
+mod state_equal_to_const;
+pub use state_equal_to_const::*;
 
-mod flip_flop_filter;
-pub use flip_flop_filter::*;
+mod state_equal_to_value;
+pub use state_equal_to_value::*;
 
-mod count_filter;
-pub use count_filter::*;
+mod state_increment_by_const;
+pub use state_increment_by_const::*;
 
-mod u8_filter;
-pub use u8_filter::*;
+mod state_increment_by_value;
+pub use state_increment_by_value::*;
 
-pub const U8_INCREMENT_STATE_FILTER: u16 = 0x0000;
-pub const U16_IS_EQUAL_FILTER_CODE: u16 = 0x0001;
-pub const U32_IS_EQUAL_STATE_FILTER_CODE: u16 = 0x0002;
-pub const U32_INCREMENT_STATE_FILTER_CODE: u16 = 0x0003;
-pub const U32_SET_STATE_FILTER_CODE: u16 = 0x0004;
-pub const FLIP_FLOP_FILTER_CODE: u16 = 0x0005;
-pub const COUNT_FILTER_CODE: u16 = 0x0006;
+mod set_state_to_const;
+pub use set_state_to_const::*;
 
-pub const BOOL_IS_EQUAL_STATE_FILTER_CODE: u16 = 0x0008;
-pub const BOOL_SET_STATE_FILTER_CODE: u16 = 0x0009;
-pub const U8_IS_EQUAL_FILTER_CODE: u16 = 0x000a;
-pub const U8_SET_STATE_FILTER_CODE: u16 = 0x000b;
-pub const U8_SET_STATE_FROM_VALUE_FILTER_CODE: u16 = 0x000c;
-pub const BOOL_FLIP_STATE_FILTER_CODE: u16 = 0x000d;
+mod set_state_to_value;
+pub use set_state_to_value::*;
+
+mod flip_state;
+pub use flip_state::*;
+
+pub const VALUE_EQUAL_TO_CONST_FILTER_CODE: u16 = 0x0000;
+pub const STATE_EQUAL_TO_CONST_FILTER_CODE: u16 = 0x0001;
+pub const STATE_EQUAL_TO_VALUE_FILTER_CODE: u16 = 0x0002;
+pub const STATE_INCREMENT_BY_CONST_FILTER_CODE: u16 = 0x0003;
+pub const STATE_INCREMENT_BY_VALUE_FILTER_CODE: u16 = 0x0004;
+pub const SET_STATE_TO_CONST_FILTER_CODE: u16 = 0x0005;
+pub const SET_STATE_TO_VALUE_FILTER_CODE: u16 = 0x0006;
+pub const FLIP_STATE_FILTER_CODE: u16 = 0x0007;
 
 #[derive(Debug, PartialEq)]
 pub enum FilterError {
--- src/filter/state/mod.rs
+++
@@ -1,8 +0,0 @@
-mod u8_state_filter;
-pub use u8_state_filter::*;
-
-mod u32_state_filter;
-pub use u32_state_filter::*;
-
-mod bool_state_filter;
-pub use bool_state_filter::*;
